2323d4f3dc5459c54abdc8edf72cf8a2f41af98c
ankur.singhal <ankur.singhal@iris-csg-675.irissoftware.com>
2017-05-24 18:34:27 +0530

    Merge branch 'master' of https://github.com/codethinker09/codeExamples.git
    survey project

---
+++ survey/src/main/java/org/ankur/survey/controller/UserController.java
@@ -0,0 +1,35 @@
+package org.ankur.survey.controller;
+
+import org.ankur.survey.dao.UserDaoImpl;
+import org.ankur.survey.entity.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+	@Autowired
+	UserDaoImpl userDaoImpl;
+
+	@RequestMapping(value = "/addUser", method = RequestMethod.GET)
+	public String addUsers(@RequestParam(value = "userName") String userName ,
+			@RequestParam(value = "userDetails") String userDetails) {
+
+		User user = new User();
+		user.setUserName(userName);
+		user.setUserDetails(userDetails);
+
+		userDaoImpl.saveOrUpdate(user);
+		return "success";
+	}
+
+	@RequestMapping(value = "/testUser/{id}", method = RequestMethod.GET)
+	public String testUser(@PathVariable String id) {
+		return id;
+	}
+}
---
+++ survey/src/main/java/org/ankur/survey/dao/UserDao.java
@@ -0,0 +1,5 @@
+package org.ankur.survey.dao;
+
+public interface UserDao {
+
+}
