d313430c3bb25b2ee6168ff15da75bc4bd7832a7
ankur.singhal <ankur.singhal@iris-csg-675.irissoftware.com>
2017-05-31 10:47:08 +0530

    code changes, csv issue

--- survey/src/main/java/org/ankur/survey/controller/CSVFileDownloadController.java
+++ survey/src/main/java/org/ankur/survey/controller/CSVFileDownloadController.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import org.ankur.survey.pojo.SearchSurveyRequest;
 import org.ankur.survey.pojo.SearchSurveyResponse;
 import org.ankur.survey.service.AdminService;
+import org.apache.commons.lang.StringEscapeUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,14 +25,12 @@ public class CSVFileDownloadController {
 	private AdminService adminServiceImpl;
 
 	@RequestMapping(value = "/downloadCSV")
-	public void downloadCSV(HttpServletResponse response,
-			@RequestBody SearchSurveyRequest searchSurveyRequest)
+	public void downloadCSV(HttpServletResponse response, @RequestBody SearchSurveyRequest searchSurveyRequest)
 			throws IOException {
 
 		response.setContentType("text/csv");
 		String reportName = "Service_rating.csv";
-		response.setHeader("Content-disposition", "attachment;filename="
-				+ reportName);
+		response.setHeader("Content-disposition", "attachment;filename=" + reportName);
 
 		List<String> rows = new ArrayList<String>();
 		rows.add("Person Name,Service Name,Service Rating,Service Time Rating,Feedback,Optional issue link,Date");
@@ -41,13 +40,13 @@ public class CSVFileDownloadController {
 				.fetchSurveyDataFilterBased(searchSurveyRequest);
 
 		for (SearchSurveyResponse searchSurveyResponse : searchSurveyResponses) {
-			rows.add(searchSurveyResponse.getUser() + ","
-					+ searchSurveyResponse.getIssueType() + ","
-					+ searchSurveyResponse.getServicerating() + ","
-					+ searchSurveyResponse.getServicetimetating() + ","
-					+ searchSurveyResponse.getFeedback() + ","
-					+ searchSurveyResponse.getOptional() + ","
-					+ searchSurveyResponse.getCreatedDate());
+			rows.add(StringEscapeUtils.escapeCsv(searchSurveyResponse.getUser()) + ","
+					+ StringEscapeUtils.escapeCsv(searchSurveyResponse.getIssueType()) + ","
+					+ StringEscapeUtils.escapeCsv(searchSurveyResponse.getServicerating()) + ","
+					+ StringEscapeUtils.escapeCsv(searchSurveyResponse.getServicetimetating()) + ","
+					+ StringEscapeUtils.escapeCsv(searchSurveyResponse.getFeedback()) + ","
+					+ StringEscapeUtils.escapeCsv(searchSurveyResponse.getOptional()) + ","
+					+ StringEscapeUtils.escapeCsv(searchSurveyResponse.getCreatedDate()));
 			rows.add("\n");
 		}
 
