9a7791a512921a3f01f02aae9e2bdf3e8763fbe5
getankur86@gmail.com <getankur86@gmail.com>
2017-10-05 17:21:58 +0530

    deleted

--- jerseyClient/src/main/java/com/jerseyClient/Helper.java
+++
@@ -1,9 +0,0 @@
-package com.jerseyClient;
-
-public class Helper {
-
-	public static String getTagValue(String xml, String tagName) {
-		return xml.split("<" + tagName + ">")[1].split("</" + tagName + ">")[0];
-	}
-
-}
